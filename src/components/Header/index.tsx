db80678ac4b18e3a2f33de24be656f4a38e49027
FaioMonzani <renderbros@gmail.com>
2021-01-11 22:02:28 +0000

    AccountElement styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -106,11 +106,12 @@ const AccountElement = styled.div<{ active: boolean }>`
   display: flex;
   flex-direction: row;
   align-items: center;
-  background-color: ${({ theme, active }) => (!active ? theme.bg1 : theme.bg3)};
+  background-color: #FDEAF1;
   border-radius: 12px;
   white-space: nowrap;
   width: 100%;
   cursor: pointer;
+  color: #FF007A;
 
   :focus {
     border: 1px solid blue;
